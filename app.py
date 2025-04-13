cc5e60f1e1593621c549f953be5e5b8b7a2ea7be
Sreenivasulu1914 <sreenivasulupanasabotla@gmail.com>
2025-04-13 17:49:32 +0530

    Update app.py for static files and PDF download

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, flash, jsonify,url_for, redir
 from flask_mail import Mail, Message
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'your-secret-key'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
